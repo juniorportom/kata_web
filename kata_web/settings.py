8c62a4c8709d781b79eb2ebcb65f5a86b52b57fc
Reinaldo Portocarrero <juniorportom@gmail.com>
2018-10-07 12:09:49 -0500

    Se devuelve configuracion base de datos

--- kata_web/settings.py
+++ kata_web/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'kata_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME', ''),
-        'USER': os.environ.get('DB_USER', ''),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOSTNAME', ''),
-        'PORT': os.environ.get('DB_PORT', ''),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
