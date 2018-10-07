eb4de67aba002014f896e9eec55813a8f9ad7b93
Reinaldo Portocarrero <juniorportom@gmail.com>
2018-10-07 12:39:11 -0500

    Rojo: buscar titulo Busco ayuda

--- independent/Functional_Test.py
+++ independent/Functional_Test.py
@@ -6,8 +6,13 @@ from selenium.webdriver.common.by import By
 class FunctionalTest(TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome(os.environ['CHROME_DRIVER'])
+        # self.browser = webdriver.Chrome(os.environ['CHROME_DRIVER'])
+        self.browser = webdriver.Chrome('/Users/juniorportom/projects/pruebas/chromedriver')
         self.browser.implicitly_wait(2)
 
     def tearDown(self):
         self.browser.quit()
+
+    def test_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('BuscoAyuda', self.browser.title)
